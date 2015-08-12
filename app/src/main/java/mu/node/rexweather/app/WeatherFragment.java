80c0413b7d27f5700bd790e324c49fa9cafcb111
Ralph Pina <ralph@cotap.com>
2015-08-12 11:14:58 -0700

    Removed adapter and recycler view for candidates to fill

--- app/src/main/java/mu/node/rexweather/app/WeatherFragment.java
+++ app/src/main/java/mu/node/rexweather/app/WeatherFragment.java
@@ -6,8 +6,6 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,7 +14,6 @@ import android.widget.TextView;
 
 import org.apache.http.HttpException;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -26,9 +23,6 @@ import butterknife.Bind;
 import butterknife.ButterKnife;
 import de.keyboardsurfer.android.widget.crouton.Crouton;
 import de.keyboardsurfer.android.widget.crouton.Style;
-import mu.node.rexweather.app.Helpers.DayFormatter;
-import mu.node.rexweather.app.Helpers.DividerItemDecoration;
-import mu.node.rexweather.app.Helpers.TemperatureFormatter;
 import mu.node.rexweather.app.Models.CurrentWeather;
 import mu.node.rexweather.app.Models.WeatherForecast;
 import mu.node.rexweather.app.Services.LocationService;
@@ -60,12 +54,12 @@ public class WeatherFragment extends Fragment {
 
     @Bind(R.id.swipe_refresh_container)
     SwipeRefreshLayout mSwipeRefreshLayout;
-    @Bind(R.id.weather_forecast_list)
-    RecyclerView       mForecastRecyclerView;
+    // TODO Add RecyclerView
     @Bind(R.id.attribution)
     TextView           mAttributionTextView;
 
-    private WeatherForecastListAdapter mAdapter;
+    // TODO Adapter
+//    private WeatherForecastListAdapter mAdapter;
     private CompositeSubscription mCompositeSubscription;
 
     @Override
@@ -76,13 +70,7 @@ public class WeatherFragment extends Fragment {
         ButterKnife.bind(this, rootView);
 
         // Set up list view for weather forecasts.
-        mAdapter = new WeatherForecastListAdapter();
-        mForecastRecyclerView.addItemDecoration(new DividerItemDecoration(WeatherApplication.get(),
-                                                                          DividerItemDecoration.VERTICAL_LIST));
-        mForecastRecyclerView.setHasFixedSize(true);
-        final RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(WeatherApplication.get());
-        mForecastRecyclerView.setLayoutManager(layoutManager);
-        mForecastRecyclerView.setAdapter(mAdapter);
+        // TODO Add RecyclerView
 
         mAttributionTextView.setVisibility(View.INVISIBLE);
 
@@ -110,124 +98,7 @@ public class WeatherFragment extends Fragment {
         ButterKnife.unbind(this);
     }
 
-    /**
-     * Provides items for our list view.
-     */
-    public class WeatherForecastListAdapter extends RecyclerView.Adapter<WeatherForecastListAdapter.ViewHolder> {
-
-        private List<WeatherForecast> mWeatherForecasts;
-        private CurrentWeather        mCurrentWeather;
-
-        public WeatherForecastListAdapter() {
-            this.mWeatherForecasts = new ArrayList<WeatherForecast>();
-        }
-
-        public void updateData(List<WeatherForecast> forecasts) {
-            mWeatherForecasts = forecasts;
-            notifyDataSetChanged();
-        }
-
-        public void updateCurrentWeather(CurrentWeather currentWeather) {
-            mCurrentWeather = currentWeather;
-            notifyDataSetChanged();
-        }
-
-        @Override
-        public int getItemViewType(int position) {
-            if (mCurrentWeather != null && position == 0) {
-                return WEATHER_HEADER;
-            } else {
-                return WEATHER_ITEM;
-            }
-        }
-
-        @Override
-        public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int viewType) {
-            switch (viewType) {
-                case WEATHER_HEADER:
-                    View header = LayoutInflater.from(viewGroup.getContext())
-                                                .inflate(R.layout.weather_forecast_list_header,
-                                                         viewGroup,
-                                                         false);
-                    return new HeaderViewHolder(header);
-                default:
-                    View item = LayoutInflater.from(viewGroup.getContext())
-                                              .inflate(R.layout.weather_forecast_list_item,
-                                                       viewGroup,
-                                                       false);
-                    return new ItemViewHolder(item);
-            }
-        }
-
-        @Override
-        public void onBindViewHolder(ViewHolder viewHolder, int i) {
-            viewHolder.configure(i);
-        }
-
-        @Override
-        public int getItemCount() {
-            return mWeatherForecasts.size() + (mCurrentWeather != null ? 1 : 0);
-        }
-
-        public class ItemViewHolder extends ViewHolder {
-
-            @Bind(R.id.day)
-            TextView dayTextView;
-            @Bind(R.id.description)
-            TextView descriptionTextView;
-            @Bind(R.id.maximum_temperature)
-            TextView maximumTemperatureTextView;
-            @Bind(R.id.minimum_temperature)
-            TextView minimumTemperatureTextView;
-
-            public ItemViewHolder(View itemView) {
-                super(itemView);
-                ButterKnife.bind(this, itemView);
-            }
-
-            @Override
-            public void configure(int position) {
-                final WeatherForecast weatherForecast = mWeatherForecasts.get(position - (mCurrentWeather != null ? 1 : 0));
-                final DayFormatter dayFormatter = new DayFormatter(getActivity());
-                final String day = dayFormatter.format(weatherForecast.getTimestamp());
-                dayTextView.setText(day);
-                descriptionTextView.setText(weatherForecast.getDescription());
-                maximumTemperatureTextView.setText(
-                        TemperatureFormatter.format(weatherForecast.getMaximumTemperature()));
-                minimumTemperatureTextView.setText(
-                        TemperatureFormatter.format(weatherForecast.getMinimumTemperature()));
-            }
-        }
-
-        public class HeaderViewHolder extends ViewHolder {
-
-            @Bind(R.id.location_name)
-            TextView locationName;
-            @Bind(R.id.current_temperature)
-            TextView currentTemp;
-
-            public HeaderViewHolder(View itemView) {
-                super(itemView);
-                ButterKnife.bind(this, itemView);
-            }
-
-            @Override
-            public void configure(int position) {
-                locationName.setText(mCurrentWeather.getLocationName());
-                currentTemp.setText(
-                        TemperatureFormatter.format(mCurrentWeather.getTemperature()));
-            }
-        }
-
-        public abstract class ViewHolder extends RecyclerView.ViewHolder {
-
-            public ViewHolder(View itemView) {
-                super(itemView);
-            }
-
-            public abstract void configure(int position);
-        }
-    }
+    // TODO Add an adapter!!!
 
     /**
      * Get weather data for the current location and update the UI.
@@ -294,12 +165,14 @@ public class WeatherFragment extends Fragment {
                                                    // Update UI with current weather.
                                                    final CurrentWeather currentWeather = (CurrentWeather) weatherData
                                                            .get(KEY_CURRENT_WEATHER);
-                                                   mAdapter.updateCurrentWeather(currentWeather);
+                                                   // TODO add data to Adapter
+//                                                   mAdapter.updateCurrentWeather(currentWeather);
 
                                                    // Update weather forecast list.
                                                    @SuppressWarnings("unchecked") final List<WeatherForecast> weatherForecasts = (List<WeatherForecast>)
                                                            weatherData.get(KEY_WEATHER_FORECASTS);
-                                                   mAdapter.updateData(weatherForecasts);
+                                                   // TODO add data to Adapter
+//                                                   mAdapter.updateData(weatherForecasts);
                                                }
 
                                                @Override
